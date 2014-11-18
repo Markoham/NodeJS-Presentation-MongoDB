fad38d724196d94f2219eb7754d12897c38cb39d
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-11-18 14:15:44 +0200

    mongo

--- example/models/user.js
+++ example/models/user.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
 
+var dbUrl = 'mongodb://kayttaja2:sala2@104.131.167.239:27017/data';
+var connection = mongoose.createConnection(dbUrl);
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', function () {
+  console.info('connected to database');
+});
+
+
 var userSchema = mongoose.Schema({
 	waId	        : String,
     access          : Boolean,
